2385c3dfa470ccbb09310a5a6297456d0f9b5990
yashbhardwaj786 <$104vinita405&>
2020-12-21 19:55:54 +0530

    Code Up

---
+++ app/src/main/java/com/enablex/demoenablex/web_communication/WebConstants.kt
@@ -0,0 +1,22 @@
+package com.enablex.demoenablex.web_communication
+
+object WebConstants {
+
+    /* To try the app with Enablex hosted service you need to set the kTry = true */
+            val   kTry = true;
+    /*Your webservice host URL, Keet the defined host when kTry = true */
+        val kBaseURL = "https://demo.enablex.io/"
+
+    /*The following information required, Only when kTry = true, When you hosted your own webservice remove these fileds*/
+
+    /*Use enablec portal to create your app and get these following credentials*/
+             val kAppId = "5fa952c6a38e36491c544e62";
+                val kAppkey = "aaeSyuasySyGyzyWaZy9aDyTuea6ygaAeDy2";
+
+    val getRoomId = "createRoom/"
+    val getRoomIdCode = 1
+    val validateRoomId = "getRoom/"
+    val validateRoomIdCode = 2
+    val getTokenURL = "createToken/"
+    val getTokenURLCode = 3
+}
